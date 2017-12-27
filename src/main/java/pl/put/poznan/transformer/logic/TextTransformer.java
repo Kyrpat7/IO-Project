eb685b00091eaba4b44ff0cd8c68aa199c6957e8
Patryk Stasiewski <patryk_stasiewski@wp.pl>
2017-12-27 19:41:30 +0100

    Initial commit

---
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -0,0 +1,18 @@
+package pl.put.poznan.transformer.logic;
+
+/**
+ * This is just an example to show that the logic should be outside the REST service.
+ */
+public class TextTransformer {
+
+    private final String[] transforms;
+
+    public TextTransformer(String[] transforms){
+        this.transforms = transforms;
+    }
+
+    public String transform(String text){
+        // of course normally it would to something based on transforms
+        return text.toUpperCase();
+    }
+}
