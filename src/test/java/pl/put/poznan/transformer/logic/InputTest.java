0177cf61b16f04d4df5690f4721f5c06abfbb3be
wuwrut <wuwrut@gmail.com>
2017-12-29 14:19:13 +0100

    added tests stub

---
+++ src/test/java/pl/put/poznan/transformer/logic/InputTest.java
@@ -0,0 +1,41 @@
+package pl.put.poznan.transformer.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InputTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void toStringTest() {
+    }
+
+    @Test
+    public void getTitleTest() {
+    }
+
+    @Test
+    public void getActorsTest() {
+    }
+
+    @Test
+    public void getStepsTest() {
+    }
+
+    @Test
+    public void getStepsCountTest() {
+    }
+
+    @Test
+    public void getConditionalDecisionCountTest() {
+    }
+
+    @Test
+    public void getBuggableLinesTest() {
+    }
+}
