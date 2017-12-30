53ce8d81cb3491849df3dba62bd358b15701f4c5
kotlecik666 <jakub.pyra.1@gmail.com>
2017-12-30 16:56:02 +0100

    loading scenario

--- src/main/java/pl/put/poznan/transformer/app/MainWindow.java
+++ src/main/java/pl/put/poznan/transformer/app/MainWindow.java
@@ -1,8 +1,8 @@
 package pl.put.poznan.transformer.app;
 
+import pl.put.poznan.transformer.logic.Input;
+
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 
 /**
@@ -11,37 +11,46 @@ import java.io.File;
 public class MainWindow {
     private static JFileChooser fc;
     private JPanel mainPanel;
-    private JButton aleksandraButton;
+    private JButton wczytajButton;
     private JButton analizaButton;
     private JPanel bottomPanel;
     private JPanel topPanel;
-    private JList list1;
-    //private JFileChooser fc;
+    private JList<String> list1;
+    private JLabel scenarioTitle;
+    private JScrollPane scrollPane;
     private File file;
 
     public MainWindow() {
-        aleksandraButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                int returnVal = fc.showDialog(mainPanel, "Wybierz plik");
-                if (returnVal == JFileChooser.APPROVE_OPTION){
-                    file = fc.getSelectedFile();
-                    System.out.println("Otwieranie pliku " + file);
-                }
-                else
-                {
-                    System.out.println("Anulowano otwieranie pliku");
-                }
+        wczytajButton.addActionListener(actionEvent -> {
+            int returnVal = fc.showDialog(mainPanel, "Wybierz plik");
+            if (returnVal == JFileChooser.APPROVE_OPTION){
+                file = fc.getSelectedFile();
+                System.out.println("Otwieranie pliku " + file);
+                loadFile(file);
+            }
+            else
+            {
+                System.out.println("Anulowano otwieranie pliku");
             }
         });
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("MainWindow");
+        JFrame frame = new JFrame("Analizator Scenariuszy");
         frame.setContentPane(new MainWindow().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         fc = new JFileChooser();
+    }
+
+    private void loadFile(File file){
+        Input input = new Input(file.toString());
+        scenarioTitle.setText(input.getTitle());
+        DefaultListModel<String> model = new DefaultListModel<>();
+        for (String s : input.getSteps().split("\n"))
+            model.addElement(s);
+        list1.setModel(model);
     }
 }
