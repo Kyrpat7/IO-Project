44e1bd489013114e962db73737310b1f3bf86dc2
wuwrut <wuwrut@gmail.com>
2018-01-03 13:34:40 +0100

    added simple tests

--- src/test/java/pl/put/poznan/transformer/logic/InputTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/InputTest.java
@@ -2,13 +2,17 @@ package pl.put.poznan.transformer.logic;
 
 import org.junit.Before;
 import org.junit.Test;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class InputTest {
 
+    private Input input;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        input = new Input("src/test/resources/data.txt");
     }
 
     @Test
@@ -17,10 +21,16 @@ public class InputTest {
 
     @Test
     public void getTitleTest() {
+        assertEquals("Scenariusz", input.getTitle());
     }
 
     @Test
     public void getActorsTest() {
+        List<String> ret = input.getActors();
+
+        assertEquals(2, ret.size());
+        assertEquals("BIBLIOTEKARZ", ret.get(0));
+        assertEquals("SYSTEM", ret.get(1));
     }
 
     @Test
@@ -29,13 +39,20 @@ public class InputTest {
 
     @Test
     public void getStepsCountTest() {
+        assertEquals(13, input.getStepsCount());
     }
 
     @Test
     public void getConditionalDecisionCountTest() {
+        assertEquals(2, input.getConditionalDecisionCount());
     }
 
     @Test
     public void getBuggableLinesTest() {
+        List<String> ret= input.getBuggableLines();
+
+        assertEquals(2, ret.size());
+        assertEquals("Wyświetla się formularz.", ret.get(0));
+        assertEquals("\tFOR EACH egzemplarz:", ret.get(1));
     }
 }
