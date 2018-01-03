0ce0b4772ea1a1e23973cef639bc507e50546869
rabalpl <33062060+rabalpl@users.noreply.github.com>
2018-01-03 20:49:02 +0100

    merge master

--- src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
+++ src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
@@ -2,6 +2,7 @@ package pl.put.poznan.transformer.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.put.poznan.transformer.logic.Input;
 
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
@@ -9,5 +10,6 @@ public class TextTransformerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TextTransformerApplication.class, args);
+
     }
 }
