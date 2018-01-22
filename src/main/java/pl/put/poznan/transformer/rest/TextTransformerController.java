1553110312b483bd642dd31170dce38fd6b5745d
Patryk Stasiewski <patryk_stasiewski@wp.pl>
2018-01-22 22:01:40 +0100

    REST api

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -2,45 +2,49 @@ package pl.put.poznan.transformer.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.transformer.logic.Scenario;
 import pl.put.poznan.transformer.logic.TextTransformer;
 
 import java.util.Arrays;
 
 
 @RestController
-@RequestMapping("/{text}")
 public class TextTransformerController {
 
     private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
 
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
 
-        // do the transformation, you should run your logic here, below just a silly example
-        TextTransformer transformer = new TextTransformer(transforms);
-        return transformer.transform(text);
+    @RequestMapping(value = "/tolevel")
+    public String getScenarioToLevel(@RequestParam(value="scenario") String scenario, @RequestParam(value="depth") int depth) {
+        Scenario s = new Scenario(scenario);
+        return s.getSubScenarios(s.toString().split("\n"), depth);
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable String text,
-                      @RequestBody String[] transforms) {
-
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // do the transformation, you should run your logic here, below just a silly example
-        TextTransformer transformer = new TextTransformer(transforms);
-        return transformer.transform(text);
+    @RequestMapping(value = "/stepscount")
+    public String getStepsCount(@RequestParam(value="scenario") String scenario) {
+        Scenario s = new Scenario(scenario);
+        return Integer.toString(s.getStepsCount());
     }
 
+    @RequestMapping(value = "/numsteps")
+    public String getNumberedSteps(@RequestParam(value="scenario") String scenario) {
+        Scenario s = new Scenario(scenario);
+        return s.toNumberedString();
+    }
 
+    @RequestMapping(value = "/keywordscount")
+    public String getCountWithKeywords(@RequestParam(value="scenario") String scenario) {
+        Scenario s = new Scenario(scenario);
+        return Integer.toString(s.getConditionalDecisionCount());
+    }
 
+    @RequestMapping(value = "/stepswithoutactor")
+    public String getStepsWithoutActor(@RequestParam(value="scenario") String scenario) {
+        Scenario s = new Scenario(scenario);
+        String res = String.join(",", s.getBuggableLines());
+        return res;
+    }
 }
 
 
