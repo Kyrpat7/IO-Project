eae4b68c32859e170b14cd8a4575c4cc38c78ce1
wuwrut <wuwrut@gmail.com>
2018-01-22 22:10:19 +0100

    test stub

---
+++ src/test/java/pl/put/poznan/transformer/rest/TextTransformerControllerTest.java
@@ -0,0 +1,25 @@
+package pl.put.poznan.transformer.rest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class TextTransformerControllerTest {
+    TextTransformerController t;
+
+    @Before
+    public void setUp() throws Exception {
+        t = new TextTransformerController();
+    }
+
+    @Test
+    public void get() {
+        
+    }
+
+    @Test
+    public void post() {
+    }
+}
